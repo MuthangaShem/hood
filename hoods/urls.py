ee7f3236d39e8d8da564ef29fe1385fc0d4d794d
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-28 00:04:03 +0300

    add hood detail url after creation

--- hoods/urls.py
+++ hoods/urls.py
@@ -5,4 +5,6 @@ app_name = 'hoods'
 urlpatterns = [
     url(r'^$', views.ListHoods.as_view(), name='all'),
     url(r'^new/$', views.CreateHood.as_view(), name='create'),
+    url(r'businesses/in/(?P<slug>[-\w]+)/$', views.SingleNeighbourhood.as_view(), name='single'),
+
 ]
