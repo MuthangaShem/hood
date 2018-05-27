5c8021e122120a4a8f0432d9493a8ccf82d469e0
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-27 23:17:51 +0300

    edit hood creation urls

--- hoods/urls.py
+++ hoods/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'neighbourhoods'
+app_name = 'hoods'
 urlpatterns = [
     url(r'^$', views.ListHoods.as_view(), name='all'),
     url(r'^new/$', views.CreateHood.as_view(), name='create'),
