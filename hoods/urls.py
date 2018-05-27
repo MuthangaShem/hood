3a7829c477e53029ffcee028759d81ca545695de
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-27 22:20:18 +0300

    add create hood class function

--- hoods/urls.py
+++ hoods/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'neighbourhoods'
 urlpatterns = [
     url(r'^$', views.ListHoods.as_view(), name='all'),
+    url(r'^new/$', views.CreateHood.as_view(), name='create'),
 ]
