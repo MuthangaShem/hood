23fa84854e342d41bbcb4747677c18ac13c5a6ac
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-27 20:54:48 +0300

    fixed login and signup error

--- hood_watch/urls.py
+++ hood_watch/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-      url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-      url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^$', views.HomePage.as_view(), name='home'),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
