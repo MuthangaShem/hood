79aa8257ada48189745138674e91f6f6ad2011cd
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-27 20:17:43 +0300

    created accounts app

--- hood_watch/urls.py
+++ hood_watch/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+      url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+      url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
