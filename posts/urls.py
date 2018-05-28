201828c27f0a0306285bf3c27c33be5da46e5c7b
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-28 18:30:47 +0300

    make migrations

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'new/$', views.CreateBusiness.as_view(), name='create'),
     url(r'by/(?P<username>[-\w]+)', views.UserBusinesses.as_view(), name='for_user'),
     url(r'by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.BusinessDetail, name='single'),
-
+    url(r'delete/(?P<pk>\d+)/$', views.DeleteBusiness.as_view(), name='delete'),
 ]
