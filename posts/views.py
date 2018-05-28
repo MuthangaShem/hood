ec8f8dcb6b583dcce52f0883404b09c166b19923
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-28 16:33:03 +0300

    create create business class view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
 
-# Create your views here.
+from django.http import Http404
+from django.views import generic
+from braces.views import SelectRelatedMixin
+from . import models
+from . import forms
+from django.contrib.auth import get_user_model
+
+class CreateBusiness(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
+    fields = ('business_name', 'business_email', 'hood')
+    model = models.Business
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
