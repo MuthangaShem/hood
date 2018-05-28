b97dd756eb48b0cbb3caf9f871d0a1f256857d31
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-28 16:48:31 +0300

    createbusiness class view

--- posts/views.py
+++ posts/views.py
@@ -9,6 +9,24 @@ from . import models
 from . import forms
 from django.contrib.auth import get_user_model
 
+class UserBusinesses(generic.ListView):
+    model = models.Business
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.business_user = User.objects.prefetch_related('businesses').get(username__iexact=self.kwargs.get('username'))
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.business_user.businesses.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['business_user'] = self.business_user
+        return context
+
+
 class CreateBusiness(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
     fields = ('business_name', 'business_email', 'hood')
     model = models.Business
