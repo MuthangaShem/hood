de07b0a02eb14971167e032a4f0bbbb7e0b8b792
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-28 17:47:05 +0300

    create url path for showing business that were created a user

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.BusinessList.as_view(), name='all'),
     url(r'new/$', views.CreateBusiness.as_view(), name='create'),
     url(r'by/(?P<username>[-\w]+)', views.UserBusinesses.as_view(), name='for_user'),
+    url(r'by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.BusinessDetail, name='single'),
 
 ]
